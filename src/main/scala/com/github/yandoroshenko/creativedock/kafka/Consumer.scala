52d16f5675d41f5342c040180b241e8c2fd6b671
Yan Doroshenko <yan1994doroshenko@gmail.com>
2018-03-14 10:17:15 +0100

    [upd] Consume from the smallest offset

--- src/main/scala/com/github/yandoroshenko/creativedock/kafka/Consumer.scala
+++ src/main/scala/com/github/yandoroshenko/creativedock/kafka/Consumer.scala
@@ -7,7 +7,7 @@ import cakesolutions.kafka.KafkaConsumer.Conf
 import com.github.yandoroshenko.creativedock.Topic
 import com.github.yandoroshenko.creativedock.Topic._
 import com.github.yandoroshenko.creativedock.util.{Configuration, Logger}
-import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.{ConsumerRecord, OffsetResetStrategy}
 import org.apache.kafka.common.serialization.StringDeserializer
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -21,7 +21,14 @@ trait Consumer extends Logger with Configuration {
 
   protected val topic: Topic
 
-  protected val consumer = KafkaConsumer(Conf(new StringDeserializer(), new StringDeserializer(), groupId = "group"))
+  protected val consumer = KafkaConsumer(
+    Conf(
+      new StringDeserializer(),
+      new StringDeserializer(),
+      groupId = "group",
+      autoOffsetReset = OffsetResetStrategy.EARLIEST
+    )
+  )
 
   protected def watch: Unit = {
     log.info(String.format("Subscribing for topic %s", topic + ""))
